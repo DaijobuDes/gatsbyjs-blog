32e0944d7e89228c972452f7d73e4efbf21bb93b
Kate Aubrey Cellan <cellankateaubrey@gmail.com>
2020-08-11 12:59:23 +0800

    Initial commit

---
+++ content/blog/about-site/index.md
@@ -0,0 +1,17 @@
+---
+title: About this site
+date: "2020-06-23T21:54:03.284Z"
+description: "About this site"
+---
+
+
+So, about this site. It's currently running on my
+[Raspberry Pi 4B](https://www.raspberrypi.org/) and is running on
+[Gatsby JS](https://www.gatsbyjs.org/).
+
+So, my site is behind [Cloudflare's Reverse Proxy](https://www.cloudflare.com) to also hide my 
+IP address and to enable SSL (HTTPS) on the site.
+
+Also, my site has a metrics system enabled and can be viewed on [/netdata](https://d-des.ga/netdata)
+
+And I'll write posts that seems interesting in the future to be viewed in the blog.
---
+++ gatsby-config.js
@@ -0,0 +1,84 @@
+module.exports = {
+  siteMetadata: {
+    title: `Merry's Blog`,
+    author: {
+      name: `Kate Aubrey Cellan`,
+      summary: `a weeb who likes Touhou and a coder.`,
+    },
+    description: `Merry's Blog using GatsbyJS.`,
+    siteUrl: `https://d-des.ga`,
+    social: {
+      twitter: `DaijobuDesKAC`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-170485620-1`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Merry's Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
